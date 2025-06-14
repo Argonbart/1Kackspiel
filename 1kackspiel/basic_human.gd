9c1ec2066c763b352f0f50125c7677e44422b5b5
Christoph <chrisschust@gmail.com>
2025-06-14 15:15:44 +0200

    poop
    poop collision layer geändert

--- 1kackspiel/basic_human.gd
+++ 1kackspiel/basic_human.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
 
+
+@export var life = 1;
+
+
 var movement_direction: bool = true
 var random_number:int = 0
 var collision
@@ -17,12 +21,20 @@ func _physics_process(delta: float) -> void:
 	
 
 		
-	if collision:
-		var other = collision.get_collider()
+	#if collision:
+		#var other = collision.get_collider()
 		#print("Kollision mit:", other.name)
-		if other.name == "PoopBody":
-			print("KACKI ANGRIFF!")
+		#if other.name == "PoopBody":
+			#life -= 1;
+		#if life <=0:
+			#queue_free()
+	
+
 
 func _on_timer_change_direction_timeout() -> void:
 	random_number = randi_range(-5, 1) % 100
 	print(random_number)
+
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	print(area.name)
--- 1kackspiel/scripts/human_spawner.gd
+++ 1kackspiel/scripts/human_spawner.gd
@@ -8,7 +8,7 @@ func _on_timer_timeout() -> void:
 	
 	 # Sichtbereich in Pixeln
 	var screen_size = get_viewport().get_visible_rect().end.x
-	print (screen_size)
+	#print (screen_size)
 	var spawn_position = bird.position.x + screen_size / 2 + 100
 
 	# Create a new instance of the Mob scene.
