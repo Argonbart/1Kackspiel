70d0f3f064fc76d9d8b292e848009bad3c8a1678
Christoph <chrisschust@gmail.com>
2025-06-14 19:53:15 +0200

    enemy animation & umbrella
    enemy animation, changes direction, umbrella blocks poop

--- 1kackspiel/scripts/basic_human.gd
+++ 1kackspiel/scripts/basic_human.gd
@@ -8,9 +8,10 @@ extends CharacterBody2D
 
 var movement_direction: bool = true
 var random_number:int = randi_range(-5, 5) % 100
-
+var umbrella
 
 func _ready() -> void:
+	
 	if randf() < 1:
 		spawn_umbrella()
 
@@ -20,10 +21,14 @@ func _ready() -> void:
 func _physics_process(_delta) -> void: 
 	
 	if random_number > 0:
+		$AnimatedSprite2D.flip_h = true
 		move_and_collide(Vector2(-2, 0))
-		
+		if umbrella:
+			umbrella.position.x = -60
 	else:
+		$AnimatedSprite2D.flip_h = false
 		move_and_collide(Vector2(2, 0))
+		umbrella.position.x = 60
 	
 
 func _on_timer_change_direction_timeout() -> void:
@@ -42,7 +47,7 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 		
 		
 func spawn_umbrella():
-	var umbrella = umbrella_scene.instantiate()
-	umbrella.scale = Vector2(0.2, 0.2)
+	umbrella = umbrella_scene.instantiate()
+	umbrella.scale = Vector2(0.18, 0.18)
 	umbrella.position = Vector2.ZERO
 	umbrella_point.add_child(umbrella)
--- 1kackspiel/scripts/umbrella.gd
+++ 1kackspiel/scripts/umbrella.gd
@@ -4,4 +4,8 @@ extends Area2D
 
 
 func _on_area_entered(area: Area2D) -> void:
-	print("Regenschirm berührt")
+	print("Regenschirm berührt", area.name)
+	if area.name == "PoopArea":
+		print("delete Poop")
+		#area.get_parent().set("should_die", true)
+	
