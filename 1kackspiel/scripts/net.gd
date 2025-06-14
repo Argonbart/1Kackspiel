e1f523bcffdb51ce0856fb140a508a86d7714ba5
Christoph <chrisschust@gmail.com>
2025-06-15 01:08:53 +0200

    Game Over screen v1.0

--- 1kackspiel/scripts/net.gd
+++ 1kackspiel/scripts/net.gd
@@ -7,5 +7,6 @@ func _on_area_entered(area: Area2D) -> void:
 	if area.name == "BirdArea":
 		var game_over_instance = game_over_scene.instantiate()
 		add_child(game_over_instance)
-		area.get_parent().get_parent().queue_free() 	# bird despawned (game over)
+		get_tree().paused = true
+		#area.get_parent().get_parent().queue_free() 	# bird despawned (game over)
 		
