91a675a977823c93ccc7ee76c7a8bdbf2366f6c7
Alexandr Katsnelson <alex.kats@web.de>
2025-06-14 16:38:29 +0200

    Update UI + Bird assets

---
+++ 1kackspiel/scripts/ui.gd
@@ -0,0 +1,24 @@
+extends CanvasLayer
+
+
+#@export_category("Poop Color")
+#@export_color_no_alpha var color_1 = Color.BLACK
+#@export_color_no_alpha var color_2 = Color.BLACK
+#@export_color_no_alpha var color_3 = Color.BLACK
+#@export_color_no_alpha var color_4 = Color.BLACK
+#@export_color_no_alpha var color_5 = Color.BLACK
+#
+#@export_category("Poop Container")
+#@export var poop_container_1: TextureRect
+#@export var poop_container_2: TextureRect
+#@export var poop_container_3: TextureRect
+#@export var poop_container_4: TextureRect
+#@export var poop_container_5: TextureRect
+#
+#
+#func _process(delta):
+	#poop_container_1.modulate = color_1
+	#poop_container_2.modulate = color_2
+	#poop_container_3.modulate = color_3
+	#poop_container_4.modulate = color_4
+	#poop_container_5.modulate = color_5
